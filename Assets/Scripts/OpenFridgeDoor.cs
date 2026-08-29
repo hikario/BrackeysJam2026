bab5ddf1513b5704ab13d1bb49de5957f2243e9e
Heather Wright <lefthandedheather@gmail.com>
2026-08-28 17:44:09 -0700

    Computer in Apartment level, audio event refs in openfridgedoor

--- Assets/Scripts/OpenFridgeDoor.cs
+++ Assets/Scripts/OpenFridgeDoor.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using FMODUnity;
 
 public class OpenFridgeDoor : MonoBehaviour
 {
@@ -9,6 +10,10 @@ public class OpenFridgeDoor : MonoBehaviour
     private bool openDoorTop;
     private bool openDoorBottom;
     public float doorSpeed;
+
+    public EventReference doorOpenAudioEvent;
+    public EventReference doorCloseAudioEvent;
+    public EventReference fridgeAmbientAudioEvent;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -49,6 +54,8 @@ public class OpenFridgeDoor : MonoBehaviour
     
     void OpenDoorTop()
     {
+        doorOpenAudioEvent.PlayOneShot();
+        fridgeAmbientAudioEvent.Play();
         if (doorRotationTop > -60)
         {
             doorTop.transform.RotateAround(doorTop.transform.position, Vector3.up, (-1 * doorSpeed) * Time.deltaTime);
@@ -58,6 +65,8 @@ public class OpenFridgeDoor : MonoBehaviour
 
     void CloseDoorTop()
     {
+        doorCloseAudioEvent.PlayOneShot();
+        fridgeAmbientAudioEvent.Stop();
         if (doorRotationTop < 0)
         {
             doorTop.transform.RotateAround(doorTop.transform.position, Vector3.up, doorSpeed * Time.deltaTime);
@@ -72,6 +81,8 @@ public class OpenFridgeDoor : MonoBehaviour
 
     void OpenDoorBottom()
     {
+        doorOpenAudioEvent.PlayOneShot();
+        fridgeAmbientAudioEvent.Play();
         if (doorRotationBottom > -60)
         {
             doorBottom.transform.RotateAround(doorBottom.transform.position, Vector3.up, (-1 * doorSpeed) * Time.deltaTime);
@@ -81,6 +92,8 @@ public class OpenFridgeDoor : MonoBehaviour
 
     void CloseDoorBottom()
     {
+        doorCloseAudioEvent.PlayOneShot();
+        fridgeAmbientAudioEvent.Stop();
         if (doorRotationBottom < 0)
         {
             doorBottom.transform.RotateAround(doorBottom.transform.position, Vector3.up, doorSpeed * Time.deltaTime);
